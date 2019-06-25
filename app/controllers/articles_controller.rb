738e8c63cc1d786e5f731f58adb0250f5799019e
DIPANSHU SAJWAN <dsajwan98@gmail.com>
2019-06-25 14:25:08 +0530

    CRUD API

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,48 @@
 class ArticlesController<ApplicationController
 
-    
+    def create
+        @article=Article.new
+        @article.name=params[:name]
+        @article.description=params[:description]
+        if (@article.save)
+            render json: {message: 'Created successfully'}
+        else
+            render json: {message: 'Not Created'}
+        end
+    end
+
+    def index
+        @articles=Article.all
+        render json: {articles: @articles}    
+    end
+
+    def update
+        byebug
+        @article=Article.find(params[:id].to_i)
+        @article.name=params[:name]
+        @article.description=params[:description]
+        if (@article.update(article_params))
+            render json: {message: 'Updated successfully'}
+        else
+            render json: {message: 'Not Updated'}
+        end
+        
+    end
+
+    def destroy
+        @article=Article.find(params[:id].to_i)
+        if (@article.destroy)
+            render json: {message: 'Deleted successfully'}
+        else
+            render json: {message: 'Not Deleted'}
+        end
+
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:name,:description)
+    end
+
 end
